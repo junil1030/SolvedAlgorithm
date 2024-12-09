a55a0d0f8ba35e33db0b3ae7852b041ff6fa9030
Junil Seo <dccrdseo@naver.com>
2024-12-09 18:19:05 +0900

    1-I 문제 풀이 추가

---
+++ 1_Week_question/1-I/1-I.cpp
@@ -0,0 +1,36 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, m;
+map<int, string> docsInt;
+map<string, int> docsStr;
+string q[100004];
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		string pokemon;
+		cin >> pokemon;
+		docsInt.insert({ i, pokemon });
+		docsStr.insert({ pokemon, i });
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> q[i];
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		if (atoi(q[i].c_str()) == 0) //문자라면
+		{
+			cout << docsStr[q[i]] << "\n";
+		}
+		else
+		{
+			cout << docsInt[atoi(q[i].c_str())] << "\n";
+		}
+	}
+	return 0;
+}
