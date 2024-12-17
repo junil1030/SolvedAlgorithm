9dde155711a49d90e268af4dffa50718d48b234f
Junil Seo <dccrdseo@naver.com>
2024-12-17 12:32:00 +0900

    1-M 풀이 추가

---
+++ 1_Week_question/1-M/1-M.cpp
@@ -0,0 +1,27 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, cnt;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		cin >> s;
+		stack<char> stk;
+		for (char a : s)
+		{
+			if (stk.size() && stk.top() == a)
+				stk.pop();
+			else
+				stk.push(a);
+		}
+		if (stk.size() == 0)
+			cnt++;
+	}
+	cout << cnt;
+}
