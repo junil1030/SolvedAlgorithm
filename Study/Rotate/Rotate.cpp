1d9c18ba8bc311a8adf9014aa6b08d6ed8f9a829
Junil Seo <dccrdseo@naver.com>
2025-01-05 23:38:45 +0900

    1차원 배열 회전

---
+++ Study/Rotate/Rotate.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	vector<int> v = { 1, 2, 3, 4, 5, 6 };
+
+	rotate(v.begin(), v.begin() + 1, v.end());
+	//rotate(v.begin(), v.begin() + 2, v.end()); 2칸 이동	//rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5); 일부분 이동	//rotate(v.rbegin(), v.rbegin() + 1, v.rend()); 시계방향 이동
+
+	vector<int> vv = { 1, 2, 3, 4, 5, 6 };
+	int i = 1;
+	int temp = vv[i];
+	vv[i] = vv[i + 1];
+	vv[i + 1] = vv[i + 2];
+	vv[i + 2] = vv[i + 3];
+	vv[i + 3] = temp;
+	for (int i : vv) cout << i << ' ';
+
+	for (int i : v) cout << i << ' ';
+
+}
