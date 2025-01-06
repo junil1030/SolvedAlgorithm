af32f9179ff5835bd89b23bfed3e716d35a9e69e
Junil Seo <dccrdseo@naver.com>
2025-01-06 11:29:45 +0900

    누적합 개념

---
+++ Study/PrefixSum/PrefixSum.cpp
@@ -0,0 +1,26 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+int ary[100000];
+int aNum, bNum;
+int pSum[100000];
+int n, m;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> ary[i];
+		pSum[i] = pSum[i - 1] + ary[i];
+	}
+	for (int i = 0; i < m; i++) 
+	{
+		cin >> aNum >> bNum;
+		cout << pSum[bNum] - pSum[aNum] << "\n";
+	}
+	return 0;
+}
