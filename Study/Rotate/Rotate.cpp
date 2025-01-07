aac95a80da5694d90de25f81939d4cf29d7e9c26
Junil Seo <dccrdseo@naver.com>
2025-01-07 18:36:59 +0900

    오타 수정

--- Study/Rotate/Rotate.cpp
+++ Study/Rotate/Rotate.cpp
@@ -4,7 +4,9 @@ int main() {
 	vector<int> v = { 1, 2, 3, 4, 5, 6 };
 
 	rotate(v.begin(), v.begin() + 1, v.end());
-	//rotate(v.begin(), v.begin() + 2, v.end()); 2칸 이동	//rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5); 일부분 이동	//rotate(v.rbegin(), v.rbegin() + 1, v.rend()); 시계방향 이동
+	//rotate(v.begin(), v.begin() + 2, v.end()); 2칸 이동
+	//rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5); 일부분 이동
+	//rotate(v.rbegin(), v.rbegin() + 1, v.rend()); 시계방향 이동
 
 	vector<int> vv = { 1, 2, 3, 4, 5, 6 };
 	int i = 1;
@@ -13,7 +15,8 @@ int main() {
 	vv[i + 1] = vv[i + 2];
 	vv[i + 2] = vv[i + 3];
 	vv[i + 3] = temp;
-	for (int i : vv) cout << i << ' ';
+	for (int i : vv) cout << i << ' ';
+
 
 	for (int i : v) cout << i << ' ';
 
