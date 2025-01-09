9916af122678c86c8d13e2a9fb0b0577374f6640
Junil Seo <dccrdseo@naver.com>
2025-01-09 21:56:54 +0900

    최대공약수와 최소공배수

---
+++ Study/Number_Theory/Number_Theory.cpp
@@ -0,0 +1,20 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (a == 0) return b;
+	return gcd(b % a, a);
+}
+
+int lcm(int a, int b)
+{
+	return (a * b) / gcd(a, b);
+}
+
+int main() {
+	int a = 10, b = 12;
+	cout << lcm(a, b) << '\n';
+
+	return 0;
+}
