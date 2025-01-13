1b4bb8945e3d5456bf6c490ac0eba23cfbb212f8
Junil Seo <dccrdseo@naver.com>
2025-01-13 10:29:24 +0900

    그래프 탐색 알고리즘

---
+++ Study/BFS/BFS.cpp
@@ -0,0 +1,50 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+// sudo코드
+//BFS(G, u)
+//	u.visited = 1
+//	q.push(u)
+//	while(q.size())
+//		u = q.front()
+//		q.pop()
+//		for each v : G.adj[u]
+//			if v.visited == false
+//				v.visited = u.visited + 1
+//				q.push
+
+vector<int> adj[100];
+int visited[100];
+int nodeList[] = { 10,12,14,16,18,20,22,24 };
+
+void bfs(int here) {
+	queue<int> q;
+	visited[here] = true;
+	q.push(here);
+	while (!q.empty()) {
+		int here = q.front();
+		q.pop();
+		for (int there : adj[here]) {
+			if (visited[there]) continue;
+			visited[there] = visited[here] + 1;
+			q.push(there);
+		}
+	}
+}
+
+int main() {
+	adj[10].push_back(12);
+	adj[10].push_back(14);
+
+	adj[12].push_back(18);
+	adj[12].push_back(20);
+
+	adj[20].push_back(22);
+	adj[20].push_back(24);
+	bfs(10);
+	for (int i : nodeList) {
+		cout << i << " : " << visited[i] << '\n';
+	}
+	cout << "10번으로부터 24번까지 최단거리는 : " << visited[24] - 1 << '\n';
+	return 0;
+}
---
+++ Study/DFS/DFS.cpp
@@ -0,0 +1,32 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+// sudo코드
+// DFS(u, adj)
+//		u.visited = 1
+//		for u : adj[u]
+//			if(u.visited == 0)
+//				DFS(u, adj)
+
+vector<int> adj[5];
+int visited[5];
+void dfs(int u) {
+	visited[u] = 1;
+	cout << u << '\n';
+	for (int v : adj[u]) {
+		if (visited[v] == 0)
+			dfs(v);
+	}
+	cout << u << "로부터 시작된 함수가 종료되었습니다.\n";
+	return;
+}
+
+int main() {
+	adj[1].push_back(2);
+	adj[1].push_back(3);
+	adj[2].push_back(4);
+	adj[2].push_back(5);
+	adj[4].push_back(2);
+
+	dfs(1);
+}
