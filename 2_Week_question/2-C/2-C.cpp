915574b428f2e24602947dc36fcb4be25f8eeedc
Junil Seo <dccrdseo@naver.com>
2025-01-15 12:01:13 +0900

    2-C 풀이 수정

--- 2_Week_question/2-C/2-C.cpp
+++ 2_Week_question/2-C/2-C.cpp
@@ -1,24 +1,21 @@
 ﻿#include "bits/stdc++.h"
 using namespace std;
 
-int ret = 1;
-int maxRain = 0;
-int n;
+int n, a[102][102];
+bool visited[102][102];
+int maxRain = INT_MIN;
 int dy[] = { -1,0,1,0 };
 int dx[] = { 0,-1,0,1 };
-int a[102][102];
-bool visited[102][102];
+int result;
 
-void dfs(int y, int x, int num)
-{
+void dfs(int y, int x) {
 	visited[y][x] = true;
-	for (int i = 0; i < 4; i++)
-	{
+	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-		if (!visited[ny][nx] && a[ny][nx] > num)
-			dfs(ny, nx, num);
+		if (a[ny][nx] > maxRain && visited[ny][nx] == false)
+			dfs(ny, nx);
 	}
 	return;
 }
@@ -33,27 +30,26 @@ int main()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cin >> a[j][i];
+			int temp;
+			cin >> temp;
+			a[j][i] = temp;
+			maxRain = max(maxRain, temp);
 		}
 	}
-
-	for (int num = 1; num < 101; num++)
-	{
-		memset(visited, 0, sizeof(visited));
-		int cnt = 0;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (a[i][j] > num && !visited[i][j])
-				{
-					dfs(i, j, num);
-					cnt++;
+	while (maxRain--) {
+		int ret = 0;
+		fill(&visited[0][0], &visited[0][0] + 102 * 102, false);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (a[i][j] > maxRain && visited[i][j] == false) {
+					dfs(i, j);
+					ret++;
 				}
 			}
 		}
-		ret = max(ret, cnt);
+		result = max(result, ret);
 	}
-	cout << ret << '\n';
+
+	cout << result;
 	return 0;
 }
