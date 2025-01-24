673d635a4507dcb8001c15c8b431293e7dd427e1
Junil Seo <dccrdseo@naver.com>
2025-01-24 11:50:39 +0900

    2-N 풀이 추가

---
+++ 2_Week_question/2-N/2-N.cpp
@@ -0,0 +1,31 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int t;
+int ret;
+bool bRet;
+int main() {
+	ios::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		ret = 0;
+		bRet = true;
+		string s;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] == '(') ret++;
+			else if (s[j] == ')') {
+				if (ret > 0) ret--;
+				else {
+					bRet = false;
+				}
+			}
+		}
+		if(ret == 0 && bRet) cout << "YES" << '\n';
+		else {
+			cout << "NO" << '\n';
+		}
+	}
+}
