8f2c8d241a4bdff2754e7eb35ed117f9feda6853
Junil Seo <dccrdseo@naver.com>
2025-01-27 13:01:02 +0900

    feat: pointer 개념 Study

---
+++ Study/Pointer/Pointer.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+void Output(const int* p)
+{
+	int i = *p;
+	int* pInt = (int*)p;
+	*pInt = 1000;
+}
+
+struct MyStruct
+{
+	int		a;
+	float	f;
+};
+
+int main()
+{
+	int nNum{};
+
+	int* pInt = &nNum;
+
+	*pInt = 10;
+
+	printf("%d\n", nNum);
+
+	int* const pint = &nNum;
+
+	for (int i = 0; i < 10; i++)
+	{
+		*pInt += 1;
+		printf("%d\n", nNum);
+	}
+
+	Output(pInt);
+	printf("%d\n", nNum);
+
+	void* pVoid = nullptr;
+
+	char szChar[7] = "abcdef";
+
+	int a = 0;
+
+	MyStruct s{};
+
+	s.a;
+
+	MyStruct* pS = &s;
+
+	return 0;
+}
