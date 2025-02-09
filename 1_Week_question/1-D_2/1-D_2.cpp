88ccae9c48b8b82fbfdc183c414957ff5756028d
Junil Seo <dccrdseo@naver.com>
2025-02-09 14:20:31 +0900

    feat: 이전 틀린 문제 다시 풀기(D, E) / fix: B, C 코드 수정

---
+++ 1_Week_question/1-D_2/1-D_2.cpp
@@ -0,0 +1,25 @@
+﻿// BOJ 10988
+#include "bits/stdc++.h"
+using namespace std;
+
+string str;
+
+int main()
+{
+	ios::sync_with_stdio(NULL);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> str;
+
+	for (int i = 0; i < str.size() / 2; i++)
+	{
+		if (str[i] != str[str.size() - 1 - i])
+		{
+			cout << 0;
+			return 0;
+		}
+	}
+	cout << 1;
+	return 0;
+}
---
+++ 1_Week_question/1-E_2/1-E_2.cpp
@@ -0,0 +1,37 @@
+﻿// BOJ 1159
+#include "bits/stdc++.h"
+using namespace std;
+
+int ary[28];
+int num;
+
+int main()
+{
+	ios::sync_with_stdio(NULL);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		string str;
+		cin >> str;
+		ary[str[0] - 'a']++;
+	}
+
+	bool bRet = false;
+	for (int i = 0; i < 28; i++)
+	{
+		if (ary[i] >= 5)
+		{
+			char c = 'a' + i;
+			cout << c;
+			bRet = true;
+		}
+	}
+
+	if (!bRet)
+	{
+		cout << "PREDAJA";
+	}
+}
