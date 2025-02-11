c77211e6152b3c096ba56545020db91cb0343745
Junil Seo <dccrdseo@naver.com>
2025-02-11 09:05:03 +0900

    feat: 문제 복기(H, J)

---
+++ 1_Week_question/1-H_2/1-H_2.cpp
@@ -0,0 +1,24 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, k, temp;
+int ret = INT_MIN;
+vector<int> pSum;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+	pSum.push_back(0);
+	for (int i = 1; i <= n; i++) {
+		cin >> temp;
+		pSum.push_back(pSum[i - 1] + temp);
+	}
+
+	for (int i = k; i <= n; i++) {
+		ret = max(ret, pSum[i] - pSum[i - k]);
+	}
+	cout << ret;
+
+	return 0;
+}
---
+++ 1_Week_question/1-J_2/1-J_2.cpp
@@ -0,0 +1,27 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int t, n;
+map<string, int> m;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+	while (t--) {
+		m.clear();
+
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			string temp, s;
+			cin >> temp >> s;
+			m[s]++;
+		}
+
+		long long ret = 1;
+		for (auto a : m) {
+			ret *= (a.second + 1);
+		}
+		cout << ret - 1 << "\n";
+	}
+}
