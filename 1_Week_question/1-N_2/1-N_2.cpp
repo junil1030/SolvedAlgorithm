afa631cdf8ac2009dc0173be3597860a53c5b6e7
Junil Seo <dccrdseo@naver.com>
2025-02-14 09:59:31 +0900

    feat: 문제복기 (N, O)

--- 1_Week_question/1-N_2/1-N_2.cpp
+++ 1_Week_question/1-N_2/1-N_2.cpp
@@ -1,11 +1,23 @@
 ﻿#include "bits/stdc++.h"
 using namespace std;
+#define ll long long
+
+ll a, b, c;
+ll go(ll a, ll b) {
+	if (b == 1) return a % c;
+	ll ret = go(a, b / 2);
+	ret = (ret * ret) % c;
+	if (b % 2) ret = (ret * a) % c;
+	return ret;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	// 다시 풀 문제
+	cin >> a >> b >> c;
+	cout << go(a, b) << "\n";
 
 	return 0;
 }
---
+++ 1_Week_question/1-O_2/1-O_2.cpp
@@ -0,0 +1,25 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	while (cin >> n) {
+		long long cnt = 1, ret = 1;
+		while (true) {
+			if (cnt % n == 0)
+			{
+				cout << ret << '\n';
+				break;
+			}
+			else
+			{
+				cnt = (cnt * 10) + 1;
+				cnt %= n;
+				ret++;
+			}
+		}
+	}
+}
