b4a8ae839cf0d8ccaef99be9a1c417182e104db1
Junil Seo <dccrdseo@naver.com>
2025-02-15 13:07:57 +0900

    feat: 문제 복기 (A, B)

---
+++ 2_Week_question/2-A_2/2-A_2.cpp
@@ -0,0 +1,39 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, m;
+int a[102][102], visited[102][102];
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,1,0,-1 };
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			a[i][j] = int(s[j] - '0');
+		}
+	}
+
+	queue<pair<int, int>> q;
+	visited[0][0] = 1;
+	q.push({ 0,0 });
+	while (q.size()) {
+		pair<int, int> top = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = top.first + dy[i];
+			int nx = top.second + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (visited[ny][nx] || a[ny][nx] == 0) continue;
+			visited[ny][nx] = visited[top.first][top.second] + 1;
+			q.push({ ny, nx });
+		}
+	}
+
+	cout << visited[n - 1][m - 1] << "\n";
+	return 0;
+}
---
+++ 2_Week_question/2-B_2/2-B_2.cpp
@@ -0,0 +1,49 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int a[52][52], visited[52][52];
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,1,0,-1 };
+int t, n, m, ret, k;
+void dfs(int y, int x) {
+	visited[y][x] = 1;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+		if (a[ny][nx] == 0 || visited[ny][nx] == 1) continue;
+		dfs(ny, nx);
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t; 
+	int x, y;
+	while (t--) {
+		cin >> m >> n >> k;
+		fill(&a[0][0], &a[0][0] + 52 * 52, 0);
+		fill(&visited[0][0], &visited[0][0] + 52 * 52, 0);
+		ret = 0;
+		while (k--) {
+			cin >> x >> y;
+			a[y][x] = 1;
+		}
+
+		int b = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if(a[i][j] && visited[i][j] == 0) {
+					dfs(i, j);
+					ret++;
+				}
+			}
+		}
+
+		cout << ret << "\n";
+	}
+
+	return 0;
+}
