fc928163803904d2a447c68f92826f02c97af2f6
Junil Seo <dccrdseo@naver.com>
2025-03-15 22:32:59 +0900

    feat: 문제 복기 2회차 (C, F)

---
+++ 2_Week_question/2-C_2/2-C_2.cpp
@@ -0,0 +1,55 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, a[102][102];
+bool visited[102][102];
+int maxRain = INT_MIN;
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,-1,0,1 };
+int result;
+
+void dfs(int y, int x) {
+	visited[y][x] = true;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+		if (a[ny][nx] > maxRain && visited[ny][nx] == false)
+			dfs(ny, nx);
+	}
+	return;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			int temp;
+			cin >> temp;
+			a[j][i] = temp;
+			maxRain = max(maxRain, temp);
+		}
+	}
+	while (maxRain--) {
+		int ret = 0;
+		fill(&visited[0][0], &visited[0][0] + 102 * 102, false);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (a[i][j] > maxRain && visited[i][j] == false) {
+					dfs(i, j);
+					ret++;
+				}
+			}
+		}
+		result = max(result, ret);
+	}
+
+	cout << result;
+	return 0;
+}
---
+++ 2_Week_question/2-F_2/2-F_2.cpp
@@ -0,0 +1,29 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+int n, m, j, l, r, temp, ret;
+int main() {
+	ios::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m >> j;
+	l = 1;
+	for (int i = 0; i < j; i++)
+	{
+		r = l + m - 1;
+		cin >> temp;
+		if (temp >= l && temp <= r) continue;
+		else {
+			if (temp < l) {
+				ret += (l - temp);
+				l = temp;
+			}
+			else {
+				l += (temp - r);
+				ret += (temp - r);
+			}
+		}
+	}
+	cout << ret << '\n';
+	return 0;
+}
